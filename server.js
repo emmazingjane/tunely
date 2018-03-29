68e5266d0a510972602e90b92c94a17bc06a7787
emmabennett <emma.j.bennett15@gmail.com>
2018-03-29 15:49:51 -0700

    finished sprint0

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+// creating a server
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+	response.sendFile('views/index.html' , { root : __dirname});
+	// setting the root directory
+});
+
+var Tunely = require('./models');
+var controllers = require('./controllers')
+
+app.get('/api', controllers.api.index);
+
+
+
+
+
+
+
+
+
+var server = app.listen(3000)
+// can use app.listen(3000)
